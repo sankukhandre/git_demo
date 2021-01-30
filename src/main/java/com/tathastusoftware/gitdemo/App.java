43fa7ec52fdf03d84b5de020306420becfe46bec
sankukh <sankumisme@gmail.com>
2021-01-30 22:41:14 +0530

    Added my first git demo project

---
+++ src/main/java/com/tathastusoftware/gitdemo/App.java
@@ -0,0 +1,28 @@
+package com.tathastusoftware.gitdemo;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+    
+    public static void addMethodToTest()
+    {
+        System.out.println( "Call addMethodToTest() " );
+        
+    }
+    
+    public static void addMethodToTest1()
+    {
+        System.out.println( "Call addMethodToTest1() " );
+        
+    }
+}
